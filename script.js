30fca694f7e0f669fdf310bdd652fc8b20f406b8
russtyguy <128891043+russtyguy@users.noreply.github.com>
2023-12-13 00:03:39 +0500

    Add makeSquare() and some css styles
    Adds makeSquare() that populates div.container with n amount of squares
    and some css styles to see the changes.

---
+++ script.js
@@ -0,0 +1,11 @@
+const squareGrid = document.querySelector(".container");
+
+function makeSquares(num) {
+  for (let i = 0; i <= num; i++) {
+    const square = document.createElement("div");
+    square.setAttribute("class", "square");
+    squareGrid.appendChild(square);
+  }
+}
+
+makeSquares(16);
